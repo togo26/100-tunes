71a2d336d4da794fa83d4fdfd689ce6df6013409
Ethan Shin <shinhc5@gmail.com>
2021-02-13 15:47:20 +0900

    Code formatting

--- src/components/molecules/AlbumCard.tsx
+++ src/components/molecules/AlbumCard.tsx
@@ -73,7 +73,10 @@ const GradientBox = styled.div`
   width: 100%;
   height: 100%;
   opacity: 0.5;
-  background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,1) 100%);
+  background: linear-gradient(
+    180deg, rgba(0,0,0,0) 0%,
+    rgba(0,0,0,1) 100%
+  );
 `;
 
 const OuterShadowBox = styled.img`
